5e3404bc76bf7d969bab91e16b27d9bee439b5d8
Watchiiee <op7970@gachon.ac.kr>
2025-05-10 18:37:22 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
+import MainPage from "./components/MainPage";
+import NoticePage from "./components/NoticePage";
+import NoticeDetailPage from "./components/NoticeDetailPage";
 
-export default function App() {
-  return <div>App</div>;
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/notices" element={<NoticePage />} />
+        <Route path="/notices/:id" element={<NoticeDetailPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
+
+// 초기 설정 필수!!!
+// npm install react-router-dom
+// npm install react-icons
+// npm install react-datepicker date-fns
